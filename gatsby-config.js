d327f006450b934ae51b1dd20ce02b4e641a38c6
Prashant Sani <contact@prashantsani.com>
2020-10-04 22:48:00 +0530

    gatsby + tailwind___ basic set-up

---
+++ gatsby-config.js
@@ -0,0 +1,49 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Demo`,
+    description: ``,
+    author: `@prashantsani`,
+    siteUrl: `https://prashantsani.github.io/react-table/`,
+    socialImage: ``
+  },
+  // pathPrefix: "",
+  // assetPrefix: "",
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-minify-classnames`,
+      options: {
+        dictionary: 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ0123456789',
+        enableOnDevelopment: false,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-no-sourcemaps",
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Montserrat\:300,300i,400,400i,700,700i` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    },
+  ],
+}
